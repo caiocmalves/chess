085179cd3196cdeb559ce21487a1e2e73a7b1cf7
Caio Carvalho <caio.cmalves@gmail.com>
2022-08-15 15:01:41 -0300

    camada chess e imprimindo o board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3, 5);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
-        System.out.println(pos);
     }
 }
